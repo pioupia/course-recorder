b5cbd898c8bcc8a04b30314c65120bc583a42a7f
pioupia <49518790+pioupia@users.noreply.github.com>
2022-08-15 16:30:04 +0200

    fix: speed in km per hour and not in meters per second (#27)

--- app/src/main/java/fr/pioupia/courserecorder/BackgroundService.java
+++ app/src/main/java/fr/pioupia/courserecorder/BackgroundService.java
@@ -157,8 +157,6 @@ public class BackgroundService extends Service {
             double slope = 0;
             actualSpeed = location.getSpeed();
 
-
-
             altMetric = location.getAltitude();
             speed += actualSpeed;
             speedCount++;
