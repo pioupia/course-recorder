f26099fa07b94cdcfd282eb404022b27b9bebb0b
pioupia <pioupiadeux@gmail.com>
2022-08-15 16:39:00 +0200

    refactor: remove unnecessary comments Co-authored-by: Pierrick-L <49518790+pioupia@users.noreply.github.com>

--- app/src/main/java/fr/pioupia/courserecorder/Array.java
+++ app/src/main/java/fr/pioupia/courserecorder/Array.java
@@ -4,18 +4,11 @@ public class Array {
     private long[] arr;
     public int count;
 
-    // Note they can only be called through function
-
-    // Method 1
-    // Inside helper class
-    // to compute length of an array
     public Array(int length) { arr = new long[length]; }
 
     public long[] toArray() {
         long[] tab = new long[count];
-        for (int i = 0; i < count; i++) {
-            tab[i] = arr[i];
-        }
+        System.arraycopy(arr, 0, tab, 0, count);
         return tab;
     }
 
@@ -23,24 +16,14 @@ public class Array {
         return arr[index];
     }
 
-    // Method 3
-    // Inside Helper class
     public void push(long element)
     {
 
         if (arr.length == count) {
-
-            // Creating a new array double the size
-            // of array declared above
             long[] newArr = new long[2 * count];
 
-            // Iterating over new array using for loop
-            for (int i = 0; i < count; i++) {
-                newArr[i] = arr[i];
-            }
+            System.arraycopy(arr, 0, newArr, 0, count);
 
-            // Assigning new array to original array
-            // created above
             arr = newArr;
         }
 
